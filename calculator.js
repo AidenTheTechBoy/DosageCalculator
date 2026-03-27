b35b9aeec3de5994fb515c2b81d744365802a408
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2026-03-27 19:36:07 -0400

    Fix calculations and add difference

--- calculator.js
+++ calculator.js
@@ -1,12 +1,8 @@
 const LBS_TO_KG = 0.45359237;
 
-function calculateDailyDosageMg(weightLbs, dosageMgPerKg) {
+function calculateDosage(weightLbs, dosageMgPerKg) {
     const weightKg = weightLbs * LBS_TO_KG;
-    return dosageMgPerKg * weightKg;
-}
-
-function calculateIndividualDosage(dailyDosageMg, frequencyPerDay) {
-    return dailyDosageMg / frequencyPerDay;
+    return weightKg * dosageMgPerKg;
 }
 
 function calculateNumberOfPills(dosageMg, pillSizeMg) {
@@ -17,11 +13,6 @@ function roundToNearestHundredth(num) {
     return Math.round(num * 100) / 100;
 }
 
-const dailyDosageMg = calculateDailyDosageMg(18.2, 25)
-const individualDosageMg = calculateIndividualDosage(dailyDosageMg, 2)
-const dailyPills = calculateNumberOfPills(dailyDosageMg, 50)
-const individualPills = calculateNumberOfPills(individualDosageMg, 50)
-console.log(roundToNearestHundredth(dailyDosageMg) + " mg daily")
-console.log(roundToNearestHundredth(individualDosageMg) + " mg per dose")
-console.log(roundToNearestHundredth(dailyPills) + " pills per day")
-console.log(roundToNearestHundredth(individualPills) + " pills per dose")
+function roundToNearestQuarter(number) {
+    return Math.round(number * 4) / 4;
+}
