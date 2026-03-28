2e3d70f333ffed64d1936aa7f9d3f54b147d567e
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2026-03-27 20:11:34 -0400

    Make calculations more transparent

--- calculator.js
+++ calculator.js
@@ -1,15 +1,47 @@
 const LBS_TO_KG = 0.45359237;
 
 function lbsToKg(lbs) {
-    return lbs * LBS_TO_KG;
+    return {
+        units: 'kg',
+        result: lbs * LBS_TO_KG,
+        calculation: `${rnd(lbs)} lbs * ${rnd(LBS_TO_KG)}`
+    };
 }
 
 function calculateDosage(weightKg, dosageMgPerKg) {
-    return weightKg * dosageMgPerKg;
+    return {
+        units: 'mg',
+        result: weightKg * dosageMgPerKg,
+        calculation: `${rnd(weightKg)} kg * ${rnd(dosageMgPerKg)} mg/kg`
+    };
 }
 
 function calculateNumberOfPills(dosageMg, pillSizeMg) {
-    return dosageMg / pillSizeMg;
+    return {
+        units: 'pills',
+        result: dosageMg / pillSizeMg,
+        calculation: `${rnd(dosageMg)} mg / ${rnd(pillSizeMg)} mg`
+    };
+}
+
+function calculateActualDosage(pillCount, dosageMgPerPill) {
+    return {
+        units: 'mg',
+        result: pillCount * dosageMgPerPill,
+        calculation: `${rnd(pillCount)} pills * ${rnd(dosageMgPerPill)} mg`
+    };
+}
+
+function calculateDifference(expectedDosage, actualDosage) {
+    return {
+        units: 'mg',
+        result: expectedDosage.result - actualDosage.result,
+        calculation: `${rnd(expectedDosage.result)} mg - ${rnd(actualDosage.result)} mg`
+    };
+}
+
+function rnd(num) {
+    return Math.round(num * 1000) / 1000;
 }
 
 function roundToNearestHundredth(num) {
